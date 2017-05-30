86c9da2ae34dac0bcf616b25f4cbfce7d8d264c9
Tom Solacroup <tom.solacroup@gmail.com>
2017-05-30 16:43:18 +0200

    Pushing barcode scanning with phone native camera app

--- js/quagga_file_input.js
+++ js/quagga_file_input.js
@@ -11,17 +11,6 @@ $(function() {
                     App.decode(URL.createObjectURL(e.target.files[0]));
                 }
             });
-
-            $(".controls .reader-config-group").on("change", "input, select", function(e) {
-                e.preventDefault();
-                var $target = $(e.target),
-                    value = $target.attr("type") === "checkbox" ? $target.prop("checked") : $target.val(),
-                    name = $target.attr("name"),
-                    state = self._convertNameToState(name);
-
-                console.log("Value of "+ state + " changed to " + value);
-                self.setState(state, value);
-            });
         },
         _accessByPath: function(obj, path, val) {
             var parts = path.split('.'),
@@ -41,9 +30,7 @@ $(function() {
             });
         },
         detachListeners: function() {
-            $(".controls input[type=file]").off("change");
-            $(".controls .reader-config-group").off("change", "input, select");
-            $(".controls button").off("click");
+            $("#photoBtn").off("change");
         },
         decode: function(src) {
             var self = this,
@@ -103,9 +90,15 @@ $(function() {
             },
             decoder: {
                 readers: [{
-                    format: "code_128_reader",
-                    config: {}
-                }]
+                            format: "ean_reader",
+                            config: {
+                                //enable the supplements for ean-extended reader
+                                //
+                                // supplements: [
+                                //     'ean_5_reader', 'ean_2_reader'
+                                // ]
+                            }
+                        }]
             },
             locate: true,
             src: null
@@ -136,37 +129,49 @@ $(function() {
     }
 
     Quagga.onProcessed(function(result) {
-        var drawingCtx = Quagga.canvas.ctx.overlay,
-            drawingCanvas = Quagga.canvas.dom.overlay,
-            area;
+        //you can put some code here to draw shapes, which can help debugging things out
 
-        if (result) {
-            if (result.boxes) {
-                drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-                result.boxes.filter(function (box) {
-                    return box !== result.box;
-                }).forEach(function (box) {
-                    Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
-                });
-            }
+    //     var drawingCtx = Quagga.canvas.ctx.overlay,
+    //         drawingCanvas = Quagga.canvas.dom.overlay,
+    //         area;
 
-            if (result.box) {
-                Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
-            }
+    //     if (result) {
+    //         if (result.boxes) {
+    //             drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+    //             result.boxes.filter(function (box) {
+    //                 return box !== result.box;
+    //             }).forEach(function (box) {
+    //                 Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+    //             });
+    //         }
 
-            if (result.codeResult && result.codeResult.code) {
-                Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
-            }
+    //         if (result.box) {
+    //             Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "#00F", lineWidth: 2});
+    //         }
 
-            if (App.state.inputStream.area) {
-                area = calculateRectFromArea(drawingCanvas, App.state.inputStream.area);
-                drawingCtx.strokeStyle = "#0F0";
-                drawingCtx.strokeRect(area.x, area.y, area.width, area.height);
-            }
-    }
+    //         if (result.codeResult && result.codeResult.code) {
+    //             Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+    //         }
+
+    //         if (App.state.inputStream.area) {
+    //             area = calculateRectFromArea(drawingCanvas, App.state.inputStream.area);
+    //             drawingCtx.strokeStyle = "#0F0";
+    //             drawingCtx.strokeRect(area.x, area.y, area.width, area.height);
+    //         }
+    // }
     });
 
     Quagga.onDetected(function(result) {
-        alert(result.codeResult.code)
+        alert(result.codeResult.code);
+        clearFileInput(document.getElementById("photoBtn"));
     }); 
 });
+
+function clearFileInput(ctrl) {
+  try {
+    ctrl.value = null;
+  } catch(ex) { }
+  if (ctrl.value) {
+    ctrl.parentNode.replaceChild(ctrl.cloneNode(true), ctrl);
+  }
+}
